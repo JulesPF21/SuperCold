55d2b578393c45493fff03edcfa41ad3a03c8866
Luanqpa <rastello.boulot@gmail.com>
2025-11-05 10:27:34 +0100

    throwable objet sys

---
+++ Assets/Script/FlashBang.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class FlashBang : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
